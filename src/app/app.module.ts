2f3f692be8adad90ed0a35b90f1ec6e178d792df
Honorato <felipe.honorato@bonobotec.com.br>
2022-12-15 17:21:19 -0300

    initial commit
    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+
+import { NgxPaginationModule } from 'ngx-pagination';
+
+import { MatSortModule } from '@angular/material/sort';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
+import { SearchPipe } from '../app/service/search.pipe';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxPaginationModule,
+    MatSortModule,
+    MatProgressBarModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [SearchPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
